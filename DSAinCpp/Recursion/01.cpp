f11849780b971c3b9d522e0470f3c1b9f644f788
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-09-17 13:24:39 -0400

    Recusion 01

---
+++ DSAinCpp/Recursion/01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+string repertname(string s, int n) {
+    if (n == 0) {
+        return "";
+    }
+    cout << s << endl;
+    return s + repertname(s, n - 1);
+}
+int main() {
+    repertname("Aditya", 5);
+    return 0;
+}
