238ed1001641ae5384690dc43a2f77accb3d3513
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-09-18 23:12:50 -0400

    Recusion 02

--- DSAinCpp/Recursion/02.cpp
+++ DSAinCpp/Recursion/02.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 using namespace std;
-
+int countnum(int i, int n) {
+    if (i == n+1) {
+        return 0;
+    }
+    cout << i << endl;
+    return countnum(i+1,n);
+}
 int main() {
-    
+    countnum(1, 5);
     return 0;
 }
