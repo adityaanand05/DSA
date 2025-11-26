870aa8afcda240fe0daa0efdcc710fa58e2b644d
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-11-26 10:30:18 +0530

    selection 01

---
+++ DSAinCpp/Recursion/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> s;
+    s = {1, 2, 3, 4, 5};
+    int k = s.size();
+    cout << k << endl;
+    return 0;
+}
