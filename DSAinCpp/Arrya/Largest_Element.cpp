b75294f5786e50a2f47377678026cf69aa9fdaf8
Aditya Anand <aditya19study@gmail.com>
2026-04-13 02:03:12 +0530

    Second Largest Element in Array

--- DSAinCpp/Arrya/Largest_Element.cpp
+++ DSAinCpp/Arrya/Largest_Element.cpp
@@ -1,27 +1,29 @@
-//Find the Largest Element in an Array
-
+// Find the Largest Element in an Array
 
 #include <iostream>
-#include<vector>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
     int largest = arr[0];
-    for(int j=0;j<n;j++){
-        cin>>arr[j];
+    for (int j = 0; j < n; j++)
+    {
+        cin >> arr[j];
     }
 
-    for(int i=0;i<n;i++){
-        if(largest<arr[i])
+    for (int i = 0; i < n; i++)
+    {
+        if (largest < arr[i])
             largest = arr[i];
     }
     cout << largest;
     return 0;
 }
 
-// Watch my solution on gfg 
-// https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+// Watch my solution on gfg
+// https://www.geeksforgeeks.org/profile/adityaanand05
--- DSAinCpp/Arrya/Second_Largest_Element.cpp
+++ DSAinCpp/Arrya/Second_Largest_Element.cpp
@@ -1,24 +1,37 @@
-//Find the Second Largest Element in an unsorted Array
-
+// Find the Second Largest Element in an unsorted Array
 
 #include <iostream>
-#include<vector>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
     int largest = arr[0];
-    for(int j=0;j<n;j++){
-        cin>>arr[j];
+    int second = -1;
+    for (int j = 0; j < n; j++)
+    {
+        cin >> arr[j];
     }
 
-    for(int i=0;i<n;i++){
-        if(largest<arr[i])
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            second = largest;
             largest = arr[i];
+        }
+        else if (arr[i] > second && arr[i] != largest)
+        {
+            second = arr[i];
+        }
     }
-    cout << largest;
+    cout << second;
     return 0;
 }
+
+// Watch my solution on gfg
+// https://www.geeksforgeeks.org/profile/adityaanand05
